7117fd767b40b5e9e79667fa6aaaeb568dc3dfba
Raelies <126143755+Raelies@users.noreply.github.com>
2025-04-08 21:50:36 +1000

    Update EventListener.java

--- src/main/java/com/devConnor/betterFortune/listeners/EventListener.java
+++ src/main/java/com/devConnor/betterFortune/listeners/EventListener.java
@@ -6,16 +6,11 @@ import com.devConnor.betterFortune.managers.FortuneManager;
 import lombok.Builder;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Random;
-
-import static com.devConnor.betterFortune.utils.VersionUtils.getFortuneEnchant;
 
 @Builder
 public class EventListener implements Listener {
@@ -29,6 +24,11 @@ public class EventListener implements Listener {
         Block block = e.getBlock();
         Player player = e.getPlayer();
 
+        // Skip processing if the block is blacklisted
+        if (fortuneManager.isMaterialBlacklisted(block.getType())) {
+            return;
+        }
+
         if (block.getDrops().isEmpty()) {
             return;
         }
@@ -36,34 +36,18 @@ public class EventListener implements Listener {
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
         Material material = block.getType();
         Double playerBlockDupe = blockDupeManager.getPlayerBlockDupe(player.getUniqueId());
-        if ((!doesItemHaveFortune(itemInHand) && playerBlockDupe == null) || fortuneManager.isMaterialBlacklisted(material)) {
+
+        if (!doesItemHaveFortune(itemInHand) && playerBlockDupe == null) {
             return;
         }
 
-        double probabilityThreshold = fortuneManager.getProbabilityThreshold(material);
-
         e.setDropItems(false);
         for (ItemStack drop : block.getDrops()) {
-            int newAmountToDrop = calculateAmountToDrop(itemInHand.getEnchantmentLevel(getFortuneEnchant()), probabilityThreshold, playerBlockDupe);
-            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(drop.getType(), newAmountToDrop));
+            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(drop.getType()));
         }
-    }
-
-    private int calculateAmountToDrop(int fortuneLevel, double probabilityThreshold, Double playerBlockDupe) {
-        Random random = new Random();
-        playerBlockDupe = playerBlockDupe != null ? playerBlockDupe : 1;
-        int amount = 1;
-
-        for (int i = 0; i < fortuneLevel; i++) {
-            if (random.nextDouble(1) <= probabilityThreshold) {
-                amount++;
-            }
-        }
-
-        return (int) (amount * playerBlockDupe);
     }
 
     private boolean doesItemHaveFortune(ItemStack item) {
-        return item.containsEnchantment(getFortuneEnchant());
+        return item.containsEnchantment(org.bukkit.enchantments.Enchantment.LOOT_BONUS_BLOCKS);
     }
 }
